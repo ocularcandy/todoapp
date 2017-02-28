8ada4234048c1f2a59cf0888b55d1a985e4dfc44
Amber Hayes <amber@ocularcandy.biz>
2017-02-28 17:09:45 -0600

    finish up CRUD

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -38,6 +38,13 @@ class TodosController < ApplicationController
     @todos = Todo.all
   end
 
+  def destroy
+    @todo = Todo.find(params[:id])
+    @todo.destroy
+    flash[:notice] = "Todo deleted!"
+    redirect_to todos_path
+  end
+
   private
 
   def todo_params
